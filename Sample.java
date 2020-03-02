7b6709f6efdd45a555c26803a9ba92ec888aec41
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-03-01 17:47:26 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -6,4 +6,9 @@ Time Complexity :O(n)
 Space Complexity :O(n)
 Did this code successfully run on Leetcode :Yes
 
+Appraoch 2: using two node pointers and calculating the length of the LL and their diff
+
+Time Complexity :O(n)
+Space Complexity :O(1)
+Did this code successfully run on Leetcode :Yes
 
